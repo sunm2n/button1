c84f141a48098a14d8fd45cdd0edc679a49ff9c9
sunm2n <134378502+sunm2n@users.noreply.github.com>
2024-09-23 17:40:46 +0900

    first commit

--- button1.py
+++ button1.py
@@ -2,12 +2,11 @@ from tkinter import *
 
 window =Tk()
 
-b1 = Button(window, text="Yeonseung Ryu")
-b2 = Button(window, text="60231010")
+b1 = Button(window, text="sunmin lee")
+b2 = Button(window, text="602222119")
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
 
 
-print("")
 
 window.mainloop()
